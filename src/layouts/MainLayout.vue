bb968ecb6a807498243c714d4f642cbce2af06ad
yaroslav <yaroslav_filippov_2017@mail.ru>
2022-07-25 14:21:01 +0500

    testing menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,6 +3,7 @@
     <GreetingSection />
     <WhySection />
     <MapSection />
+    <MapMenu/>
   </q-layout>
 </template>
 
@@ -11,6 +12,7 @@ import { defineComponent } from "vue";
 import GreetingSection from "src/sections/GreetingSection";
 import WhySection from "src/sections/WhySection";
 import MapSection from "src/sections/MapSection";
+import MapMenu from "components/Map/MapMenu/MapMenu";
 
 export default defineComponent({
   name: "MainLayout",
@@ -18,6 +20,7 @@ export default defineComponent({
     GreetingSection,
     WhySection,
     MapSection,
+    MapMenu,
   }
 });
 </script>
